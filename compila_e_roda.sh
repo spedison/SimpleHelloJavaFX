4dcc23cfef7563afaea4a9941d8897bb172347ce
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-04-12 20:11:00 -0300

    Add details for download and use JavaFX

--- compila_e_roda.sh
+++ compila_e_roda.sh
@@ -1,3 +1,8 @@
+# For install JavaFX
+## Instruction for download : https://gluonhq.com/products/javafx/
+## Instruction for use : https://openjfx.io/openjfx-docs/#install-javafx
+
+
 #!/bin/bash
 export JAVA_FX_DIR=~/libs/javaFX
 javac --module-path $JAVA_FX_DIR --add-modules javafx.controls HelloWorld.java 
